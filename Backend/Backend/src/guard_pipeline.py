9f072b0e6aa53042ee0c266ffd61446c89ab5233
riddhi2309 <khandelwalriddhi522@gmail.com>
2026-04-06 15:43:05 +0530

    Fresh start - clean commit

--- Backend/Backend/src/guard_pipeline.py
+++
@@ -1,9 +0,0 @@
-from guard_llm import GuardLLM
-
-class FullPipeline:
-    def __init__(self, hf_token):
-        self.guard_llm = GuardLLM(hf_token)
-
-    def check(self, text):
-        result = self.guard_llm.check(text)
-        return result
